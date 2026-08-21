7022e3d5f8d00e49d991406d4e5a494b20109b7c
Samuel Jesus de Sousa <samueljesus1402208@gmail.com>
2026-08-21 18:18:36 -0300

    wip: Inserção de escolha da Criação da Tabela

--- geradores/cadastro_cliente.py
+++ geradores/cadastro_cliente.py
@@ -18,30 +18,29 @@ conexao = psycopg2.connect(
     host = os.getenv("DB_HOST"),
     port = os.getenv("DB_PORT")
 )
-cursor = conexao.cursor()
-qt_clientes = int(input("Quantos Clientes gostaria de gerar?"))
-q_ultimo_id_cliente = 'SELECT max(id_cliente) FROM clientes'
-cursor.execute(q_ultimo_id_cliente)
-ultimo_id_cliente = cursor.fetchone()[0]
-base_id = 0 if ultimo_id_cliente is None else ultimo_id_cliente
-for i in range(qt_clientes):
- id_cliente = base_id + 1 + i
-    
- nome = fake.company()
- cidade = random.choice(municipios)
- nome_cidade = cidade['nome']
- estado = cidade['microrregiao']["mesorregiao"]['UF']['sigla']
- email = fake.company_email()
- data = date.today()
- ativo = random.choice([True, False])
 
- query_inserir_cliente = "insert into clientes (id_cliente, nome, cidade,estado, email, data_cadastro,ativo) " \
- "values (%s, %s, %s, %s, %s, %s, %s)"
+def cadastrar_clientes(): 
+ cursor = conexao.cursor()
+ qt_clientes = int(input("Quantos Clientes gostaria de gerar?"))
+ q_ultimo_id_cliente = 'SELECT max(id_cliente) FROM clientes'
+ cursor.execute(q_ultimo_id_cliente)
+ ultimo_id_cliente = cursor.fetchone()[0]
+ base_id = 0 if ultimo_id_cliente is None else ultimo_id_cliente
+ for i in range(qt_clientes):
+  id_cliente = base_id + 1 + i
+  nome = fake.company()
+  cidade = random.choice(municipios)
+  nome_cidade = cidade['nome']
+  estado = cidade['microrregiao']["mesorregiao"]['UF']['sigla']
+  email = fake.company_email()
+  data = date.today()
+  ativo = random.choice([True, False])
+
+  query_inserir_cliente = "insert into clientes (id_cliente, nome, cidade,estado, email, data_cadastro,ativo) " \
+  "values (%s, %s, %s, %s, %s, %s, %s)"
 
  cursor.execute(query_inserir_cliente, (id_cliente, nome, nome_cidade,estado, email, data, ativo))
 
-conexao.commit()
-
-cursor.close()
-
-conexao.close()
+ conexao.commit()
+ cursor.close()
+ conexao.close()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-from geradores import criacao_tabelas as tab
+from geradores import criacao_tabelas as tab, cadastro_cliente as clien, cadastro_categorias as categ, cadastro_produtos as prod,cadastro_pedido as ped, cadastro_itens_pedidos as itped 
 from dados import dados_produtos as d_prod
 
 conexao = psycopg2.connect(
@@ -13,6 +13,18 @@ conexao = psycopg2.connect(
 )
 
 
-print("=== Gerador e Injetor de Dados | Supabase ===")
-tab.criar_tabelas()
+print("=== Gerador e Injetor de Dados | Supabase ===\n")
+while True: 
+ print("Gostaria de Criar as tabelas dentro do seu Banco no Supabase?\n1-Sim\n2-Não")
+ try:
+    escolha = int(input(""))
+    if escolha == 1:
+     tab.criar_tabelas
+    elif escolha == 2:
+     print("Ok!!")
+    else:
+      print("Insira um número de uma das opções")
+ except ValueError:
+   print("Insira um número")
+
 
