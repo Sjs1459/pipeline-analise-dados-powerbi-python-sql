55be44fa6206fe890a19de1e6f82aef942f85f32
Samuel Jesus de Sousa <samueljesus1402208@gmail.com>
2026-08-21 11:44:51 -0300

    wip: Modificação do Cadastro de produtos para inserção de escolha de tipo de produto, e a criação do Cadastro de categorias

--- geradores/cadastro_produtos.py
+++ geradores/cadastro_produtos.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 from dotenv import load_dotenv 
 import random
-from dados_produtos import p_hospitalar 
+from dados.dados_produtos import p_hospitalar, p_eletronicos, p_esportes
 
 load_dotenv()
 conexao = psycopg2.connect(
@@ -18,6 +18,11 @@ cursor.execute("SELECT MAX(id_produto) FROM produtos")
 ultimo_id = cursor.fetchone()[0]
 base_id = 0 if ultimo_id is None else ultimo_id
 
+try:
+ print("Você gostaria de inserir qual tipo de produtos?\n1 - Produtos Hospitalares\n2 - Produtos Eletrônicos\n 3 - Produtos Esportivos")
+ int(input())
+except ValueError:
+ print("Insira o número das opções acima")
 qt_produtos = len(p_hospitalar)
 for i in range(qt_produtos):
  id_produto = base_id + 1 + i
