47151ca0cc1d9546c937a5a1aa23853af908f935
Samuel Jesus de Sousa <samueljesus1402208@gmail.com>
2026-08-21 13:51:42 -0300

    wip: Atualização Cadastro de Produtos, inserção de função para cadastro de tipo de produtos

--- geradores/cadastro_produtos.py
+++ geradores/cadastro_produtos.py
@@ -18,32 +18,36 @@ cursor.execute("SELECT MAX(id_produto) FROM produtos")
 ultimo_id = cursor.fetchone()[0]
 base_id = 0 if ultimo_id is None else ultimo_id
 
-while True:
- try:
-  print("Você gostaria de inserir qual tipo de produtos?\n1 - Produtos Hospitalares\n2 - Produtos Eletrônicos\n 3 - Produtos Esportivos")
-  escolha_produtos = int(input())
-  if escolha_produtos == 1:
-   dados_produtos = p_hospitalar
-  elif escolha_produtos == 2:
-   dados_produtos = p_eletronicos
-  elif escolha_produtos == 3:
-   dados_produtos = p_esportes
-  else:
-   print("Informe o número das opções acima")
-  break
- except ValueError:
-  print("Informe um número")
-qt_produtos = len(dados_produtos)
-for i in range(qt_produtos):
- id_produto = base_id + 1 + i
- nome_produto = dados_produtos[i][0]
- id_categoria = dados_produtos[i][1]
- preco = dados_produtos[i][2]
- estoque = random.randint(1,300)
+def cadastrar_produtos():
+ while True:
+  try:
+   print("Você gostaria de inserir qual tipo de produtos?\n1 - Produtos Hospitalares\n2 - Produtos Eletrônicos\n3 - Produtos Esportivos")
+   escolha_produtos = int(input())
+   if escolha_produtos == 1:
+    dados_produtos = p_hospitalar
+    break
+   elif escolha_produtos == 2:
+    dados_produtos = p_eletronicos
+    break
+   elif escolha_produtos == 3:
+    dados_produtos = p_esportes
+    break
+   else:
+    print("Informe o número das opções acima")
+  
+  except ValueError:
+   print("Informe um número")
+  qt_produtos = len(dados_produtos)
+  for i in range(qt_produtos):
+   id_produto = base_id + 1 + i
+   nome_produto = dados_produtos[i][0]
+   id_categoria = dados_produtos[i][1]
+   preco = dados_produtos[i][2]
+   estoque = random.randint(1,300)
+  q_inserir_produtos = "INSERT into produtos(id_produto, nome_produto, id_categoria, preco, estoque) VALUES (%s,%s,%s,%s,%s)" 
+  cursor.execute(q_inserir_produtos, (id_produto, nome_produto, id_categoria, preco, estoque))
 
 
-q_inserir_produtos = "INSERT into produtos(id_produto, nome_produto, id_categoria, preco, estoque) VALUES (%s,%s,%s,%s,%s)" 
-cursor.execute(q_inserir_produtos, id_produto, nome_produto, id_categoria, preco, estoque )
 
 conexao.commit()
 cursor.close()
